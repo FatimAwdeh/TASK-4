29a2770fa3498bf098f3a057a6353e7f2d5c9a32
Fatima Awdeh <fatimaawdeh21@gmai.com>
2023-07-26 10:24:57 +0300

    Done with task 4

---
+++ Task-4/arrays.js
@@ -0,0 +1,8 @@
+var tableOfNumbers = Array()
+tableOfNumbers= [1,7,8,4,3,0,9,5]
+var tableOfStrings = []
+tableOfStrings = ['pomme','banane','cerise']
+var tableMixte = [6,'y',84,'ahmad3',12,9]
+console.log(tableOfNumbers)
+console.log(tableOfStrings)
+console.log(tableMixte)
